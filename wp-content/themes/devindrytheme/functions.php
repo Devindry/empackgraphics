cafe14b180bd3396cb8a2670711757db712c13db
helozano <helozano.sistemas@gmail.com>
2015-05-08 23:13:47 -0500

    segundo intento de mandar cambios de slide en servicios

--- wp-content/themes/devindrytheme/functions.php
+++ wp-content/themes/devindrytheme/functions.php
@@ -1,10 +1,11 @@
 <?php
 /* Codigo para agregar las imagenes que ocupa el blog (142 x 142) */
 add_image_size('blog-mini', 150, 150, true);
+add_image_size('blog-titulo', 500, 150, true);
 add_filter( 'image_size_names_choose', 'blogThumb' );
 function blogThumb( $sizes ) {
 	return array_merge( $sizes, array(
-		'blog-mini' => __( 'Miniatura para Blog' )
+		'blog-mini' => __( 'Miniatura para Blog' ), 'blog-titulo' => __( 'Titulo para Blog' )
 	) );
 }
 wp_enqueue_style( 'less-style', get_stylesheet_directory_uri() . 'assets/column5.less' );
